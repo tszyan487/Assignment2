2fb58248a545207e73b80dea07f5a75709087b2e
tszyan487 <tszyan487@gmail.com>
2016-08-08 22:57:10 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,13 @@
 ## functions do
 
 ## Write a short comment describing this function
+# The first function, makeVector creates a special "matrix", 
+# which is really a matrix containing a function to
+# 1.	set the value of the matrix
+# 2.	get the value of the matrix
+# 3.	set the value of the inverse of square 
+# 4.	get the value of the inverse of square
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -35,3 +42,24 @@ cacheSolve <- function(x) {
   x$setsqrt(m)
   m
 }
+
+# b <- matrix(c(1,4,9,16,25,36),nrow = 2,ncol=3)
+
+# > b
+#       [,1] [,2] [,3]
+# [1,]    1    9   25
+# [2,]    4   16   36
+
+# > a <- makeCacheMatrix(b)
+# > cacheSolve(a)
+#       [,1] [,2] [,3]
+# [1,]    1    3    5
+# [2,]    2    4    6
+
+
+# > a$get()
+#       [,1] [,2] [,3]
+# [1,]    1    9   25
+# [2,]    4   16   36
+
+
